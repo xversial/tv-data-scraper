e7f3c0062de7c6c7fdcddc1702737cddcfe3a3c5
Xversial <xversial@vionox.com>
2022-09-13 21:03:16 -0700

    Enable caching on Spring boot starting method

--- deployment/tomcat-war/src/main/java/com/vionox/tools/tvscraper/SpringBootTomcatApplication.java
+++ deployment/tomcat-war/src/main/java/com/vionox/tools/tvscraper/SpringBootTomcatApplication.java
@@ -8,13 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.vionox.tools.tvscraper"})  // force scan JPA entities
 @EnableConfigurationProperties
 @SpringBootConfiguration
+@EnableCaching
 @EnableJpaRepositories(basePackages = {"com.vionox.tools.tvscraper"})
 public class SpringBootTomcatApplication extends SpringBootServletInitializer
 {
