a143db34cfe5115383e2d01fa3d3d170a4ced999
Xversial <xversial@vionox.com>
2022-09-13 20:58:19 -0700

    Set json property order and added no-args constructor

--- components/scraper/tv-data/src/main/java/com/vionox/tools/tvscraper/data/model/plotpoint/SoundbarPoint.java
+++ components/scraper/tv-data/src/main/java/com/vionox/tools/tvscraper/data/model/plotpoint/SoundbarPoint.java
@@ -1,8 +1,10 @@
 package com.vionox.tools.tvscraper.data.model.plotpoint;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +13,10 @@ import java.io.Serializable;
 
 @Data
 @SuperBuilder
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonPropertyOrder({ "frequencyHz","response1dBr"})
 public class SoundbarPoint extends FrequencyPlotPoint implements Serializable
 {
     private static final Logger LOG = LoggerFactory.getLogger(SoundbarPoint.class);
