c41893204b8df14e429e036b8f4786ed366bf37c
Xversial <xversial@vionox.com>
2022-09-13 21:29:27 -0700

    Add method to get graph url to ScraperWebFetch

--- components/scraper/tv-data/src/main/java/com/vionox/tools/tvscraper/data/scraping/ScraperWebFetch.java
+++ components/scraper/tv-data/src/main/java/com/vionox/tools/tvscraper/data/scraping/ScraperWebFetch.java
@@ -18,14 +18,22 @@ public class ScraperWebFetch
     {
         try
         {
-            return Jsoup.connect(url)
-                    .userAgent("Mozilla/5.0").timeout(10 * 1000)
-                    .get();
+            return Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(10 * 1000).get();
         } catch (IOException e)
         {
             LOG.warn(e.getMessage());
             Sentry.captureException(e);
         }
         return null;
+    }
+
+    public String getGraphURL(int deviceID, int graphID)
+    {
+        return getGraphURL(String.valueOf(deviceID), String.valueOf(graphID));
+    }
+    public String getGraphURL(String deviceID, String graphID)
+    {
+        LOG.trace("Building URL for graph {} of device {}", graphID, deviceID);
+        return String.format("https://www.rtings.com/graph/data/%s/%s", deviceID, graphID);
     }
 }
